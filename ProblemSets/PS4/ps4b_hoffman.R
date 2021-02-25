f0ff676a3fc06e8dc9a8dc766bb4a92f51098d10
Abigail Hoffman <70658518+abbyhhoffman@users.noreply.github.com>
2021-02-25 16:55:43 -0600

    Add files via upload

--- ProblemSets/PS4/ps4b_hoffman.R
+++ ProblemSets/PS4/ps4b_hoffman.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(sparklyr)
 library(dplyr)
 
+install_spark(version = "3.0.0")
 sc <- spark_connect(master = "local")
 
 
@@ -13,16 +14,18 @@ head(df1)
 
 df <- copy_to(sc, df1)
 
-class(df1)
-class(df)
+print(class(df1))
+print(class(df))
 
 
 
 df %>% select(Sepal_Length,Species) %>% head %>% print
+df1 %>% select(Sepal_Length,Species) %>% head %>% print
 
 df %>% filter(Sepal_Length>5.5) %>% head %>% print
+df1 %>% filter(Sepal_Length>5.5) %>% head %>% print
 
-df %>% filter(species == Sepal_Length>5.5) %>% head %>% print
+df %>% filter(Sepal_Length>5.5) %>% select(Sepal_Length,Species) %>% head %>% print
 
 
 df2 <- df %>% group_by(Species) %>% summarize(mean = mean(Sepal_Length),
